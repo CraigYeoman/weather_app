28fd1d8c1e9cddb79d9070980934e5b479416c6e
CraigYeoman <yeomancraig@gmail.com>
2022-07-11 06:55:25 -0500

    Working on displaying data from API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const cityButton = document.getElementById("button");
 
+
+
 function getCity() {
   const city = document.getElementById("city");
   const location = city.value;
@@ -13,16 +15,39 @@ async function getWeather(location) {
       { mode: "cors" }
     );
     const dataWeather = await response.json();
-    console.log(dataWeather.main.temp);
+    return dataWeather
   } catch (error) {
     console.log(error);
   }
 }
 
+function displayData(dataWeather) {
+const weatherDescription = document.querySelector("[data-description]");
+const weatherTemp = document.querySelector("[data-temp]");
+const weatherTempFeel = document.querySelector("[data-feels-like");
+const weatherMin = document.querySelector("[data-min]");
+const weatherMax = document.querySelector("[data-max]");
+const weatherPressure = document.querySelector("[data-pressure]");
+const weatherHumidity = document.querySelector("[data-humidity]");
+const weatherWind = document.querySelector("[data-wind]");
+const weatherVisibility = document.querySelector("[data-visibilty]");
+
+weatherDescription.textContent = dataWeather.weather.description;
+weatherTemp.textContent = dataWeather.main.temp
+weatherTempFeel.textContent = dataWeather.main.feels_like;
+weatherMin.textContent = dataWeather.main.temp_min;
+weatherMax.textContent = dataWeather.main.temp_max;
+weatherPressure.textContent = dataWeather.main.pressure;
+weatherHumidity.textContent = dataWeather.main.humidity;
+weatherWind.textContent = dataWeather.wind.speed;
+weatherVisibility.textContent = dataWeather.visibilty;
+}
+
 function init() {
   cityButton.addEventListener("click", () => {
     const location = getCity();
-    getWeather(location);
+    const dataWeather = getWeather(location);
+    displayData(dataWeather);
   });
 }
 
